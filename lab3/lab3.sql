2410876367a4c8d47ed71d2fbafc492df202bed7
ConstantTeen <kostet-1998@mail.ru>
2019-11-22 22:49:56 +0300

    закончил 3 лабу

--- lab3/lab3.sql
+++ lab3/lab3.sql
@@ -12,12 +12,14 @@ drop view if exists secondDepEmps;
 create view secondDepEmps as
     select * from employees
         where department = 2;
-
+-- является обновляемым
 insert into secondDepEmps(name)
     values ('лёша'),
            ('виталик');
--- TODO ЗАПИЛИТЬ АПДЕЙТЫ И ИНСЕРТЫ И ОБЪЯСНИТЬ ЧТО В СКУЭЛАЙТ НЕТ ОБНОВЛЯЕМЫХ ВЬЮЕРОВ
 
+update secondDepEmps
+    set name = 'егорка'
+    where personnelNumber > 3;
 
 -- 2. Представление "Образовательный уровень сотрудников" (с указанием
 -- количества людей с высшим, средним и другими уровнями образования), по
@@ -46,10 +48,14 @@ select e.department,
     ) males
 from employees e
 group by e.department, e.education;
-
+-- не является обновляемым из-за group by как минимум
 insert into educationLevel
     values (6, 'что-то', 123,321),
            (1, 'нечто', 222,11);
+
+update educationLevel
+    set department = 101
+    where males > 3;
 
 -- 3. Представление "Вакансии": номер отдела – должность – количество вакантных ставок.
 drop view if exists vacancies;
@@ -59,7 +65,14 @@ select department, posName, ratesNumberPerDep from staffingTable
     left join positions p on
         staffingTable.position = p.posId
     order by department;
-
+-- не является обновляемым из-за left join
 insert into vacancies
     values (1, 'тырыпыры', 123),
            (123, 'кочерга', 312);
+
+update vacancies
+    set department = 101
+    where ratesNumberPerDep > 100;
+
+-- Для всех попыток использовать UPDATE или INSERT получаю подобную ошибку:
+-- [SQLITE_ERROR] SQL error or missing database (cannot modify vacancies because it is a view)
